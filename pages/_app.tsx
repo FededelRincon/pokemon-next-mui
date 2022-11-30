b7d93932437474cec0c54dc4a5e5da2cab175763
FededelRincon <delrincon@gmail.com>
2022-11-30 19:48:35 -0300

    switch theme + darkTheme + lightTheme + all set it up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
-import '../styles/globals.css'
+import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app'
 
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { darkTheme, lightTheme } from '../themes';
+
+import '../styles/globals.css'
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const [activeTheme, setActiveTheme] = useState( lightTheme )
+  const [selectedTheme, setSelectedTheme] = useState<'light' | 'dark'> ('light')
+
+  const toggleTheme = () => {
+    setSelectedTheme( selectedTheme === 'light' ? 'dark' : 'light' );
+  }
+
+  useEffect(() => {
+    setActiveTheme( selectedTheme === 'light' ? lightTheme : darkTheme )
+  }, [selectedTheme])
+  
+
+  return (
+    <ThemeProvider theme={ activeTheme }  >
+      <CssBaseline />
+      <Component {...pageProps} toggleTheme={toggleTheme} />
+    </ThemeProvider>
+
+  )
 }
