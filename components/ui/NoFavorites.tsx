1d113f69e005ae792ac61ccba5c894b4b9fe3558
FededelRincon <delrincon@gmail.com>
2022-12-30 11:07:39 -0300

    in noFavouriteComponent center all content

--- components/ui/NoFavorites.tsx
+++ components/ui/NoFavorites.tsx
@@ -4,17 +4,19 @@ import Image from "next/image"
 
 export const NoFavorites = () => {
     return (
-        <Box>
-            <Typography variant="h4">
-                There is not favourites pokemons
-            </Typography>
+        <Box sx={{ marginTop: 10 ,display: 'flex', alignItems:'center', justifyContent: 'center' }}>
+            <Box sx={{ marginBottom: 10, display: 'flex', flexDirection: 'column', justifyContent:"center", alignItems: 'center' }} >
+                <Typography variant="h4" sx={{ textAlign: 'center'}}>
+                    You have no favourites pokemons yet...
+                </Typography>
 
-            <Image 
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png"
-                alt="App Icon"
-                width={250}
-                height={250}
-            />
+                <Image 
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png"
+                    alt="App Icon"
+                    width={250}
+                    height={250}
+                />
+            </Box>
         </Box>
     )
 }
